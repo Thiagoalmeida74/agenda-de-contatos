c5e30128d5b5f20001abdb9b242c6bd3254afe6a
Thiago Almeida <thiagoalmeida74@gmail.com>
2026-08-06 13:05:52 -0300

    feat: implementa cadastro de contatos

---
+++ main.py
@@ -0,0 +1,35 @@
+contatos = []
+
+def mostrar_menu():
+    print ("=== Agenda de contatos ===")
+    print ("1 - adicionar contato")
+    print ("2 - Listar contato")
+    print ("3 - Buscar contato")
+    print ("4 - Editar contato")
+    print ("5 - Excluir contato")
+    print ("6 - Sair")
+    opcao= input("escolha uma opcao: ")
+    return opcao
+
+def adicionar_contato():
+    nome = input("Qual é seu nome?: ")
+    telefone =input("qual é seu telefone?: ")
+    email =input("qual é seu email?: ")
+    contato= {
+        "nome":nome,
+        "telefone": telefone,
+        "email": email
+        }
+    contatos.append(contato)
+    print("\n Contato adicionado com sucesso! 6\n")
+
+
+def main():
+    opcao = mostrar_menu()
+    while opcao != "6":
+        if opcao == "1":
+            adicionar_contato()
+        opcao = mostrar_menu()
+    print("até logo!")
+        
+main()
